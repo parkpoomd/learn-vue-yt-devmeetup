ac16f3a37ece22330a52966b3ce7e463e38878a2
parkpoomd <parkpoom.d@gmail.com>
2019-04-21 07:35:01 +0700

    setup firebase and a signup view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import "./plugins/vuetify";
 import App from "./App.vue";
+import * as firebase from "firebase/app";
 import router from "./router";
 import { store } from "./store";
 import DateFilter from "./filters/date";
@@ -12,5 +13,14 @@ Vue.filter("date", DateFilter);
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCm4kH5-8HHdG2kmjZU10QOJI8iF9_ek18",
+      authDomain: "devmeetup-bc45a.firebaseapp.com",
+      databaseURL: "https://devmeetup-bc45a.firebaseio.com",
+      projectId: "devmeetup-bc45a",
+      storageBucket: ""
+    });
+  }
 }).$mount("#app");
